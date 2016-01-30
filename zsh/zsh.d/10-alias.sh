c70bb874ab5b4d65cfa21e7d45c5fb3972e324c5
cormoran <cormoran707@gmail.com>
2016-01-31 01:18:50 +0900

    zsh ctrl d挙動変更

--- zsh/zsh.d/10-alias.sh
+++ zsh/zsh.d/10-alias.sh
@@ -16,6 +16,13 @@ else
     colored-echo $YELLOW "info : rmtrash does not installed."
 fi
 
+
+if type 'python2' > /dev/null || type 'python3' > /dev/null ; then
+    colored-echo $BLUE 'both python2 and 3 are installed.'
+    alias python='colored-echo $RED "please use python2 or python3"'
+    alias pip='colored-echo $RED "please use pip2 or pip3"'
+fi
+
 #alias openocd='openocd -f /usr/local/Cellar/open-ocd/0.9.0/share/openocd/scripts/board/st_nucleo_f3.cfg'
 #alias openrtm-eclipse='open ~/Programing/OpenRTM-aist/eclipse/Eclipse.app'
 #alias hoogle='open https://www.haskell.org/hoogle/'
