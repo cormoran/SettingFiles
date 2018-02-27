afff80e9ae034aa22596662022ba45d20137cdb8
cormoran <cormoran707@gmail.com>
2018-02-28 03:33:31 +0900

    add DMD and fix .config

---
+++ install_script/install_dmd.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+curl -fsS https://dlang.org/install.sh | bash -s dmd
--- linux_server_install.sh
+++ linux_server_install.sh
@@ -8,5 +8,6 @@ install_script/install_emacs.sh
 install_script/install_pyenv.sh
 install_script/install_peco.sh
 install_script/install_ghq.sh
+install_script/install_dmd.sh
 export PATH=$HOME/local/bin:$PATH
 ./install all
