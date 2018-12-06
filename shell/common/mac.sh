39139562b84a30ec3042c66706918724f3b9d069
cormoran <cormoran707@gmail.com>
2018-12-06 18:12:56 +0900

    Remove unused files

--- shell/common/mac.sh
+++
@@ -1,5 +0,0 @@
-if [ "$(uname)" = 'Darwin' ]; then
-    alias mv='gmv'
-    alias ln='gln'
-# elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-fi
--- shell/common/peco.sh
+++
@@ -1,8 +0,0 @@
-if type peco > /dev/null ; then
-    if type ghq > /dev/null ; then
-        alias g='cd `ghq root`/`ghq list | peco`'
-    fi
-    if type ghq > /dev/null ; then
-        alias gh='hub browse `ghq list | peco | cut -d "/" -f 2,3`'    
-    fi
-fi
