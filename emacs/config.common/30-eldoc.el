c407bc4f9024d97ab11f50775ee0a5092cf5fd80
cormoran <cormoran707@gmail.com>
2018-12-06 18:11:37 +0900

    Remove raw emacs config

--- emacs/config.common/30-eldoc.el
+++
@@ -1,6 +0,0 @@
-(use-package eldoc
-  :config
-  (setq eldoc-idle-delay 0.2)
-  (setq eldoc-echo-area-use-multiline-p t)
-  ;;(use-package eldoc-extension)
-  )
--- emacs/config.common/30-re.el
+++
@@ -1,9 +0,0 @@
-(use-package visual-regexp-steroids
-  :bind (
-         ("M-%" . vr/query-replace)
-         ("C-M-r" . vr/isearch-backward)
-         ("C-M-s" . vr/isearch-forward)
-         )
-  :config
-  (setq vr/engine 'python)
-  )
