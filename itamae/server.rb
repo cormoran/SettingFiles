965c4ef6267d9874c6c7b1fcfa7b15937c0bc869
cormoran <cormoran707@gmail.com>
2018-12-07 19:04:08 +0900

    add server.rb

---
+++ itamae/server.rb
@@ -0,0 +1,23 @@
+include_recipe "cookbooks/linux.rb"
+
+# shell
+include_recipe "cookbooks/shell_rc.rb"
+include_recipe "cookbooks/dotfiles.rb"
+include_recipe "cookbooks/zsh.rb"
+include_recipe "cookbooks/tmux.rb"
+include_recipe "cookbooks/bin.rb"
+
+# languages
+# include_recipe "cookbooks/rbenv.rb"
+# include_recipe "cookbooks/node_brew.rb"
+# include_recipe "cookbooks/go.rb"
+include_recipe "cookbooks/anaconda.rb"
+
+# tools
+include_recipe "cookbooks/peco.rb"
+include_recipe "cookbooks/jq.rb"
+include_recipe "cookbooks/ghq.rb"
+# include_recipe "cookbooks/code.rb"
+include_recipe "cookbooks/emacs.rb"
+
+# include_recipe "cookbooks/cica_font.rb"
