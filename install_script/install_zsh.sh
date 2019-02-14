f4866ea6c9743596c8953cc76cc4663359abcff2
cormoran <cormoran707@gmail.com>
2019-02-14 23:54:08 +0900

    update zsh install script

--- install_script/install_zsh.sh
+++ install_script/install_zsh.sh
@@ -3,21 +3,33 @@ echo """====> install zsh
 
 
      PREFIX : ${PREFIX:=$HOME/local}
-     VERSION : ${ZSH_INSTALL_VERSION:=5.4.2}
+     VERSION : ${ZSH_INSTALL_VERSION:=5.7.1}
      WORKDIR : ${MY_INSTALL_WORKDIR:=$HOME/tmp}
 
 """
 
 mkdir -p $MY_INSTALL_WORKDIR
 cd $MY_INSTALL_WORKDIR
-wget https://github.com/zsh-users/zsh/archive/zsh-${ZSH_INSTALL_VERSION}.tar.gz
-tar zxf zsh-${ZSH_INSTALL_VERSION}.tar.gz
-cd zsh-zsh-${ZSH_INSTALL_VERSION}
+
+if [ ! -e zsh-${ZSH_INSTALL_VERSION}.tar.gz ]; then
+    echo "=> downloading"
+    wget -q https://github.com/zsh-users/zsh/archive/zsh-${ZSH_INSTALL_VERSION}.tar.gz
+else
+    echo "=> downloaded file found"
+fi
+
+TMP_DIR=`mktemp -d zsh.${ZSH_INSTALL_VERSION}.XXXX`
+echo "=> working in ${TMP_DIR}"
+tar zxf zsh-${ZSH_INSTALL_VERSION}.tar.gz -C $TMP_DIR
+cd $TMP_DIR/zsh-zsh-${ZSH_INSTALL_VERSION}
 ./Util/preconfig
 mkdir build
 cd build
-../configure --prefix=$PREFIX
-make
-make install
+echo "=> configuring"
+../configure -q --prefix=$PREFIX || exit -1
+echo "=> building"
+make -j `grep -c processor /proc/cpuinfo` > make.log || exit -1
+echo "=> installing"
+make install > make.install.log
 
 echo '====> zsh OK'
