57e677cb6a215450edf974b296d5d679d6ed65cf
cormoran <cormoran707@gmail.com>
2019-04-16 06:52:00 +0900

    Update go version

--- itamae/cookbooks/go.rb
+++ itamae/cookbooks/go.rb
@@ -6,7 +6,7 @@ node.reverse_merge!({
                       :shell_rc_d => "#{node[:prefix]}/shell_rc.d",
                       :go_root => "#{node[:prefix]}/go",
                       :go_path => "#{ENV['HOME']}/Dropbox/Program",
-                      :go_version => "1.9.4"
+                      :go_version => "1.12.4"
                     })
 
 
@@ -23,14 +23,14 @@ end
 
 execute "download go" do
   cwd node[:work_dir]
-  command "wget https://dl.google.com/go/go#{node[:go_version]}.linux-amd64.tar.gz && tar -zxf go#{node[:go_version]}.linux-amd64.tar.gz"
-  not_if "test -e go"
+  command "wget https://dl.google.com/go/go#{node[:go_version]}.linux-amd64.tar.gz && tar -zxf go#{node[:go_version]}.linux-amd64.tar.gz && mv go go#{node[:go_version]}"
+  not_if "test -e go#{node[:go_version]}"
 end
 
 execute "install go" do
   cwd node[:work_dir]
-  command "cp -r go #{node[:go_root]}"
-  not_if "test -e #{node[:go_root]}"
+  command "cp -r go#{node[:go_version]} #{node[:go_root]}"
+  not_if "test -e #{node[:go_root]} && (go version | grep #{node[:go_version]})"
 end
 
 template "#{node[:shell_rc_d]}/go.sh" do
