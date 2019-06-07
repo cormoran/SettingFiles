32ec638653efdc0e961f1a2c3ff8b6a5d1f63ea3
cormoran <cormoran707@gmail.com>
2019-06-08 05:06:04 +0900

    add borg

---
+++ itamae/cookbooks/borg.rb
@@ -0,0 +1,24 @@
+# coding: utf-8
+node.reverse_merge!({
+                      :prefix => "#{ENV['HOME']}/local2",
+                      :work_dir => "#{ENV['HOME']}/tmp",
+                      :borg_version => "1.1.10",
+                      :borg_root => "#{ENV['HOME']}/Dropbox/Program/src"
+                    })
+
+
+directory node[:work_dir] do
+  action :create
+end
+
+case node[:platform]
+when 'osx', 'darwin'
+  os = 'macosx64'
+when 'debian', 'ubuntu', 'redhat'
+  os = 'linux64'
+end
+
+execute "download and install borg" do
+  command "wget https://github.com/borgbackup/borg/releases/download/#{node[:borg_version]}/borg-#{os} -O #{node[:prefix]}/bin/borg && chmod +x #{node[:prefix]}/bin/borg"
+  not_if "test -e #{node[:prefix]}/bin/borg"
+end
--- itamae/local.rb
+++ itamae/local.rb
@@ -17,6 +17,7 @@ include_recipe "cookbooks/jq.rb"
 include_recipe "cookbooks/ghq.rb"
 include_recipe "cookbooks/code.rb"
 include_recipe "cookbooks/emacs.rb"
+include_recipe "cookbooks/borg.rb"
 include_recipe "cookbooks/docker_machine.rb"
 
 include_recipe "cookbooks/cica_font.rb"
